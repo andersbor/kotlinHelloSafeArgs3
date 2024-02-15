d4fccb1c4ac5c72658966c23d0c3c170d3fd9063
andersbor <anbo@zealand.dk>
2024-02-15 08:20:38 +0100

    gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         google()
     }
     dependencies {
-        val navVersion = "2.7.6"
+        val navVersion = "2.7.7"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
